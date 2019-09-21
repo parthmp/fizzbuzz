eaed65077064484900aed402b6a0b2e77ba78f11
Parth Parmar <admin@ninjazin.com>
2019-09-22 05:15:21 +0530

    Wrap Up

--- mytask/solution/yours/Solution.php
+++ mytask/solution/yours/Solution.php
@@ -4,30 +4,52 @@
 
     class Solution{
 
-        public function generateOutPut($mode = 'default'){
+        public function generateOutPut($mode = 0){
 
             $result = '';
+            $counter = 100;
 
-            for($i=1; $i<=100; $i++){
+            if($mode == 7){
+                $counter = 110;
+            }
+
+            for($i=1; $i <= $counter; $i++){
 
                 $check_fizz = $i%3;
                 $check_buzz = $i%5;
 
+                $check_seven = $i%7;
+
                 $flag = 0;
+
+                $hold_string = '';
                 
                 if($check_fizz == 0){
+                    
                     $flag = 1;
-                    $result .= "Fizz";
-                }if($check_buzz == 0){
-                    $flag = 1;
-                    $result .= "Buzz";
-                }
-                
-                if($flag == 0){
-                    $result .= $i;
+                    $hold_string = "Fizz";
+                    
                 }
 
-                $result .= "<br />\r\n";
+                if($check_buzz == 0){
+
+                    $flag = 1;
+                    $hold_string .= "Buzz";
+                    
+                }
+                
+                if($mode == 7 && $check_seven == 0){
+
+                    $flag = 1;
+                    $hold_string .= 'Seven';
+
+                }
+
+                if($flag == 0){
+                    $hold_string .= $i;
+                }
+
+                $result .= $hold_string."<br />\r\n";
                 
             }
 
